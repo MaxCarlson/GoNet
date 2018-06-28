ea8bf250fd4155513252f915afc0b8e730f849df
Maxwell Carlson <bart4282819@gmail.com>
2018-06-28 14:31:35 -0700

    Changing over to using the CNTK framework as I was able to get the C++ side of things functional

---
+++ DataGenerator.py
@@ -0,0 +1,157 @@
+import os
+import keras
+import random
+import numpy as np
+import tensorflow as tf
+from Globals import BoardSize, BoardLength, BoardDepth, BLACK, WHITE
+#import multiprocessing, threading, queue
+
+class Generator():
+    def __init__(self, featurePath, labelPath, fileShape, batchSize):
+        self.featurePath = featurePath
+        self.labelPath = labelPath
+        self.batchSize = batchSize
+        self.fileShape = fileShape
+
+    # Set the first layer of the feature maps 
+    # to the side-to-move color code (all ones for black, 0's for white)
+    def setColorLayer(self, X, C, m):
+        for i in range(0, m):
+            X[i, 0] = C[i] - 1
+
+    # Read the data from the file created in CurateData
+    # Format it into the binary feature map explained in CurateData
+    def extractNpy(self, xFile, yFile):
+        XComp = np.load(xFile)
+        YCol  = np.load(yFile)
+        m = np.shape(YCol)[0]
+        Y = YCol[0:m, 0]
+        C = YCol[0:m, 1]
+        Y = tf.keras.utils.to_categorical(Y, BoardSize)
+
+        X = np.zeros((m, BoardDepth, BoardLength, BoardLength))
+
+        self.setColorLayer(X, C, m)
+        
+        # Build the binary feature maps for all recorded board states
+        # for each example
+        for i in range(0, m):
+            # Add both layers representing player and opponent stones
+            color = C[i]
+            opponent = WHITE if color == BLACK else BLACK
+            index = 0
+            for j in range(1, BoardDepth, 2):
+                X[i, j]   = XComp[i, index] == color
+                X[i, j+1] = XComp[i, index] == opponent
+                index += 1
+
+        return X, Y
+
+    # Use only the # of files we're told to
+    def shapeFileList(self, fileList):
+        return fileList[self.fileShape[0]:self.fileShape[1]]
+
+
+    def loadFile(self, fList, lList, indices, i):
+        sampleF = fList[indices[i]]
+        sampleL = lList[indices[i]]
+        
+        wholePathF = self.featurePath + '/' + sampleF
+        wholePathL = self.labelPath + '/' + sampleL
+
+        XX, YY = self.extractNpy(wholePathF, wholePathL)
+        return XX, YY
+
+    # Grab the next chunk of the file
+    def getNextChunk(self, XX, YY, m, roll):
+            
+            X = np.zeros((self.batchSize, BoardDepth, BoardLength, BoardLength)) # TODO: , dtype=bool
+            Y = np.zeros((self.batchSize, BoardSize))
+            
+            if roll + self.batchSize < m:
+                X = XX[roll:roll + self.batchSize]
+                Y = YY[roll:roll + self.batchSize]
+
+            # This shouldn't happen all that often,
+            # Should curate data to avoid it infact!
+            elif self.batchSize > m:
+                for b in range(self.batchSize):
+                    roll = random.randint(0, np.shape(XX)[0]-1)        
+                    X[b] = XX[roll]
+                    Y[b] = YY[roll]
+
+            else:
+                # If it's not a perfect fit, take the upper slice then the lower one
+                m1 = np.shape(XX[roll:m])[0]
+                m2 = self.batchSize - m1
+                X[0:m1] = XX[roll:m]
+                Y[0:m1] = YY[roll:m]
+                X[m1:self.batchSize] = XX[0:m2]
+                Y[m1:self.batchSize] = YY[0:m2]
+
+            roll += self.batchSize
+            if roll > m:
+                roll -= m
+
+            return X, Y, roll
+
+    # Continuously read and generate data for the model
+    # As it likely can't fit in memory
+    def generator(self):
+    
+        fList = self.shapeFileList(os.listdir(self.featurePath))
+        lList = self.shapeFileList(os.listdir(self.labelPath))
+
+        indices = np.arange(0, len(fList))
+
+        #featQ = queue()
+        #thread.start_new_thread(fillQueue, (featQ, indices, i))
+
+        i = 0
+        m = 0
+        mi = 0
+        roll = 0
+        XX, YY = np.zeros(1), np.zeros(1)
+        loadNew = True
+        # Number of mini-batches we can read from a file
+        fileLoadsPb = -1
+        while True:
+            if mi >= fileLoadsPb:
+                if i >= len(fList):
+                    i = 0
+                    random.shuffle(indices)
+                
+                # Load a new file
+                mi = 0
+                XX, YY = self.loadFile(fList, lList, indices, i)
+                m = np.shape(YY)[0]
+                fileLoadsPb = m // self.batchSize
+                # Roll for a random spot to start the batch range slice
+                roll = random.randint(0, m - 1)
+
+
+            X, Y, roll = self.getNextChunk(XX, YY, m, roll)
+
+
+            #np.swapaxes(X, 0, 3)
+            yield X, Y
+
+            mi += 1
+            if mi >= fileLoadsPb:
+                i += 1
+
+    # Count the number of samples for each file we're using
+    def stepsPerEpoch(self):
+
+        featureList = self.shapeFileList(os.listdir(self.featurePath))
+        labelList = self.shapeFileList(os.listdir(self.labelPath))
+        
+        numFiles = len(labelList)  
+        count = 0
+        for i in range(0 , numFiles):
+            featurePath = self.featurePath + '/' + featureList[i]
+            labelPath = self.labelPath + '/' + labelList[i]
+            X, Y = self.extractNpy(featurePath, labelPath)
+            count += np.shape(Y)[0]
+
+        return count / self.batchSize
--- GoNet.py
+++ GoNet.py
@@ -1,79 +1,29 @@
-import torch
+from __future__ import print_function
 import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
+import cntk as cntk
+from cntk.initializer import he_normal, normal
+from cntk.layers import MaxPooling, BatchNormalization, Convolution, Dense, Dropout
+from cntk.ops import relu
+
+from Globals import BoardDepth, BoardLength, BoardLengthP, BoardSize, BoardSizeP
+
+inputShape = (BoardDepth, BoardLength, BoardLength)
 
 
-class Net(nn.Module):
+def Conv(input, filterShape, filters,  strides=(1,1), padding=False):
+    c = Convolution(filterShape, filters, activation=None, pad=padding)
+    r = BatchNormalization(map_rank=1, normalization_time_constant=4096)
+    return relu(r)
 
-    def __init__(self):
-        super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # an affine operation: y = Wx + b
-        
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+def goNet(input, filters):
 
-    def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.softmax(self.fc3(x))
-        return x
+    conv = Conv(input, (5, 5), filters, strides=(2, 2))
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
 
-net = Net()
-net.to(device)
-print(net)
+goNet()
 
-optimizer = torch.optim.SGD(net.parameters(), lr=0.0018, momentum=0.9)
-criterion = nn.CrossEntropyLoss()
 
-epochs = 1000
-
-for epoch in range(epochs):
-    #for stuff in batch...:
-
-    dataNp = np.zeros((200, 1, 32, 32))
-    targetNp = np.ones((200))
-
-    data = Variable(torch.from_numpy(dataNp).float())
-    data = data.to(device)
-
-    # Target has to be indicies of the classification problem
-    target = Variable(torch.from_numpy(targetNp).long())
-    target = target.to(device)
-    
-
-    optimizer.zero_grad()
-    netOut = net(data)
-    loss = criterion(netOut, target)
-    print('loss', loss)
-    loss.backward()
-    optimizer.step()
-
-X = np.zeros((1, 1, 32, 32))
-X = torch.from_numpy(X).float().to(device)
-pred = net(X)
-
-print(pred)
-
-    
+def trainNet():
+    inputVar = cntk.ops.input_variable(inputShape, np.float32)
+    labelVar = cntk.ops.input_variable(BoardSize, np.float32)
