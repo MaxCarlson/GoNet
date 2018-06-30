0084b40e09ab98d9b9ac230eef3e369629db8626
Maxwell Carlson <bart4282819@gmail.com>
2018-06-29 23:27:19 -0700

    Still trying to figure out why reported accuracy is so high (acc > 99%). Much much too high

--- GoNet.py
+++ GoNet.py
@@ -18,12 +18,14 @@ labelPath = "./data/labels"
 
 def Conv(input, filterShape, filters,  strides=(1,1), padding=False):
     c = Convolution2D(filterShape, filters, activation=None, pad=padding)(input)
-    r = BatchNormalization(map_rank=1, normalization_time_constant=4096)(c)
+    b = BatchNormalization(map_rank=1, normalization_time_constant=4096)(c)
     # Add dropout
-    return relu(r)
+    return relu(b)
 
 def goNet(input, filters, outSize):
     
+    # TODO: Look into what this padding turns out to actually pad to
+    # See if we can get padding similar to AlphaGo's
     conv = Conv(input, (5, 5), filters, padding=True)
     c1  = Conv(conv, (3, 3), filters)
     c2 = Conv(c1, (3,3), filters, padding=True)
@@ -35,7 +37,7 @@ def goNet(input, filters, outSize):
 
 def trainNet():
     
-    gen = Generator(featurePath, labelPath, (0, 15), batchSize)
+    gen = Generator(featurePath, labelPath, (0, 4), batchSize)
 
     inputVar = cntk.ops.input_variable((BoardDepth, BoardLength, BoardLength), np.float32, name='features')
     labelVar = cntk.ops.input_variable(BoardSize, np.float32) #, dynamic_axes=input_dynamic_axes
@@ -43,25 +45,27 @@ def trainNet():
     net = goNet(inputVar, 64, BoardSize)
    
     # Loss and metric
-    ce = cntk.cross_entropy_with_softmax(net, labelVar)
-    pe = cntk.classification_error(net, labelVar)
+    loss = cntk.cross_entropy_with_softmax(net, labelVar)
+    #loss = cntk.squared_error(net, labelVar)
+    acc = cntk.classification_error(net, labelVar)
+    
+    minisPerBatch = gen.stepsPerEpoch()
 
-    minibatchSize = gen.stepsPerEpoch()
-
-    learner = cntk.adam(net.parameters, 0.0018, 0.9, minibatch_size=minibatchSize)
+    learner = cntk.adam(net.parameters, 0.0018, 0.9, minibatch_size=None)
 
     progressPrinter = cntk.logging.ProgressPrinter(tag='Training', num_epochs=maxEpochs)
-    trainer = cntk.Trainer(net, (ce, pe), learner, progressPrinter)
+    trainer = cntk.Trainer(net, (loss, acc), learner, progressPrinter)
 
     g = gen.generator()
     for epoch in range(maxEpochs):
+        
         miniBatches = 0
-
-        while miniBatches < batchSize:
+        while miniBatches < minisPerBatch:
             X, Y = next(g)
             miniBatches += 1 # TODO: NEED to make sure this doesn't go over minibatchSize so we're not giving innacurate #'s
 
             trainer.train_minibatch({inputVar : X, labelVar : Y})
+
        
         trainer.summarize_training_progress()
             
