f44e598c077668ea22cfb3b020c92a6544d33857
Maxwell Carlson <bart4282819@gmail.com>
2018-07-23 00:13:16 -0700

    Fixed heatmap stone coloring so now side to move isn't always black, but rather the color it actually was in the game

--- NetHeatMap.py
+++ NetHeatMap.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Globals import BoardLength
+from Globals import BoardLength, BLACK, WHITE
 import matplotlib.pyplot as plt
 import cntk
 
@@ -19,18 +19,30 @@ class NetHeatMap:
             for y in range(BoardLength):
                 exOut[x,y] = outs[0][0][y * BoardLength + x]
 
-        exOut = cntk.softmax(exOut).eval()
+        exOut         = cntk.softmax(exOut).eval()
         imgIn, imgOut = self.buildImages(X[0], exOut)
 
         fig = plt.figure(frameon=False)
         plt.imshow(imgIn)
         plt.imshow(imgOut, cmap=plt.cm.viridis, alpha=.6, interpolation='bilinear')
         plt.colorbar()
-        ticks   = np.arange((5000//BoardLength)//2, 5000, 5000//BoardLength)
+        ticks  = np.arange((5000//BoardLength)//2, 5000, 5000//BoardLength)
         labels = ('A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T')
         plt.xticks(ticks, labels)
         plt.yticks(ticks, reversed(range(1,BoardLength+1)))
         plt.show()
+
+    def buildImages(self, boards, netOut):
+        toMove  = boards[0,0,0]
+        # Build the boards with the right color stone based on stm
+        stm     = boards[1] if toMove == 0 else boards[1] + boards[1]
+        opp     = boards[2] if toMove == 1 else boards[2] + boards[2]
+        board   = stm + opp
+        boardImg    = np.zeros((BoardLength*self.shp[0],BoardLength*self.shp[1],self.shp[2]))
+        boardImg    = self.buildBoardImg(board, boardImg, toMove)
+        outImg      = self.buildOutImage(netOut)
+
+        return boardImg, outImg
 
     def addStoneToImg(self, boardImg, x, y, stoneImg):
         
@@ -38,9 +50,8 @@ class NetHeatMap:
         offY = y * self.shp[1]
         boardImg[offX:offX+self.shp[0], offY:offY+self.shp[1]] = stoneImg
 
-    def buildBoardImg(self, board, boardImg):
-        BLACK   = 1
-        WHITE   = 2
+    def buildBoardImg(self, board, boardImg, toMove):
+
         for x in range(BoardLength):
             for y in range(BoardLength):
                 img = None
@@ -65,14 +76,3 @@ class NetHeatMap:
                 offY = y * self.shp[1]
                 outImg[offX:offX+self.shp[0], offY:offY*self.shp[1]] = netOut[x,y]
         return outImg
-
-    def buildImages(self, boards, netOut):
-        # Build the nets input image (with pieces and such)
-        stm     = boards[1]
-        opp     = boards[2] + boards[2]
-        board   = stm + opp
-        boardImg    = np.zeros((BoardLength*self.shp[0],BoardLength*self.shp[1],self.shp[2]))
-        boardImg    = self.buildBoardImg(board, boardImg)
-        outImg      = self.buildOutImage(netOut)
-
-        return boardImg, outImg
